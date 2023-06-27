0f3784a1126faad2c833e5f07adba59f8d8b91cf
tizianolatino <tiziano.latino@studio.unibo.it>
2023-06-27 10:14:55 +0200

    added the function that apply node2vec on the data and the PCA

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ from negative_binomial_model import generate_data_from_neg_binomial
 from Hic_data_prepro import drop_chr, remove_isolated_bins
 from utils import reduce_dimension
 from utils_data_metadata import adjust_indices_to_zero
+from draw import plot_embedding, plot_data
+from translocated_data import generate_node_labels
+from node2vec_hic_toydataset import data_to_embedding
+
 
 def main(data, metadata):
     
@@ -25,13 +29,22 @@ def main(data, metadata):
    metadata, data = remove_isolated_bins(metadata, data)
    
    #reduce dimension
-   #data, metadata = reduce_dimension(metadata, data, 0.5)
-
+   data, metadata = reduce_dimension(metadata, data, 0.9)
 
    # Generate a new dataframe with values from a negative binomial distribution
    neg_bin_data = generate_data_from_neg_binomial(metadata, data)
-
-    
+   
+   
+   node_labels = generate_node_labels(metadata, "'chr1'", 5, 10, "'chr6'", 105,110)
+   
+   # Plot the Adajacency Matrix
+   plot_data(neg_bin_data, metadata, [0,10,100,1000,5000,10000])
+   
+   
+   embeddings = data_to_embedding(data)
+   
+   plot_embedding(embeddings, node_labels)
+   
    return neg_bin_data, metadata
 
 if __name__ == "__main__":
@@ -41,8 +54,8 @@ if __name__ == "__main__":
     metadata = pd.read_csv('Data/metadata.csv')
     
 
-    neg_bin_data, neg_bin_metadata = main(data, metadata)
+    #neg_bin_data, neg_bin_metadata = main(data, metadata)
 
-
+    
 
     
